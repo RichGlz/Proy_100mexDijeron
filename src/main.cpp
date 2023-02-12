542679bfa2449669df31013d4947bb885bfc4bcf
RichGlz <ragparkour@gmail.com>
2023-02-12 07:36:50 -0600

    Inicio de repo

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+
+
+
+void setup() {
+
+  // Buttons
+  const uint8_t btnA =             13;
+  const uint8_t btnB =             12;
+
+  const uint8_t reproduceAplausos= 14;
+  const uint8_t reproduceInicio  = 27;
+  const uint8_t reproduceInter   = 26;
+  const uint8_t reproduceBuena   = 25;
+  const uint8_t reproduceError   = 33;
+
+  /*
+  SONIDOS
+  Vamos a jugar
+  respuesta correcta
+  musica de triunfo
+  tiempo en el reloj
+  */
+
+  // LEDs
+  const uint8_t ledEncendido =    32;
+  const uint8_t ledBtnA      =    35;
+  const uint8_t ledBtnB      =    34;
+
+  // Modulos
+    // Carga de batería 
+    // TFPlayer
+    // Speaker
+    // Display_14s
+    // 
+
+}
+
+void loop() {
+
+}
